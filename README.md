4f8b28449a122f982a9ecf4a6778dbeb5e07a255
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-03-08 13:54:37 -0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# NodeTSESMTemplate
 # NodeTSESMExpressTemplate
---
+++ src/index.ts
@@ -0,0 +1,116 @@
+import * as dotenv from "dotenv"
+dotenv.config()
+
+import express from "express"
+import { v4 as uuidv4 } from "uuid"
+import cors from "cors"
+import { statfs } from "fs/promises"
+
+import { logger } from "./logger/index.js"
+
+import { extractError } from "./utils.js"
+
+const listenPort = process.env.PORT || "8080"
+
+declare global {
+  namespace Express {
+    interface Request {
+      id: string
+    }
+  }
+
+  namespace NodeJS {
+    interface ProcessEnv {
+      DB_FILENAME?: string
+      ADMIN_EMAILS?: string
+      AWS_DEFAULT_REGION?: string
+      AWS_ACCESS_KEY_ID?: string
+      AWS_SECRET_ACCESS_KEY?: string
+      S3_BUCKET: string
+      S3_ENDPOINT: string
+      POSTMARK_TOKEN: string
+      MY_URL: string
+
+      AXIOM_ORG_ID: string
+      AXIOM_TOKEN: string
+      AXIOM_DATASET: string
+    }
+  }
+}
+
+const diskCheckLoop = setInterval(async () => {
+  const stats = await statfs(process.env.DISK_PATH || "/")
+  const totalSpace = stats.bsize * stats.blocks
+  const availableSpace = stats.bsize * stats.bfree
+  if (availableSpace < totalSpace * 0.15) {
+    // When 15% space left, notify
+    logger.error("less less than 15% disk space remaining!")
+  }
+}, 30_000)
+
+process.on("unhandledRejection", (reason: any, p: Promise<any>) => {
+  logger.error(
+    {
+      err: reason instanceof Error ? extractError(reason) : reason,
+    },
+    "unhandled promise rejection"
+  )
+})
+
+async function main() {
+  const app = express()
+  app.use(express.json())
+  app.disable("x-powered-by")
+  app.use(cors())
+
+  // Remix public
+  app.use(express.static("public"))
+  app.use(express.static("src/public"))
+
+  app.use((req, res, next) => {
+    const reqID = uuidv4()
+    req.id = reqID
+    next()
+  })
+
+  if (process.env.HTTP_LOG === "1") {
+    logger.debug("using HTTP logger")
+    app.use((req: any, res, next) => {
+      req.log.info({ req })
+      res.on("finish", () => req.log.info({ res }))
+      next()
+    })
+  }
+
+  app.get("/hc", (req, res) => {
+    res.sendStatus(200)
+  })
+
+  const server = app.listen(listenPort, () => {
+    logger.info(`API listening on port ${listenPort}`)
+  })
+
+  const signals = {
+    SIGHUP: 1,
+    SIGINT: 2,
+    SIGTERM: 15,
+  }
+
+  let stopping = false
+  Object.keys(signals).forEach((signal) => {
+    process.on(signal, async () => {
+      if (stopping) {
+        return
+      }
+      stopping = true
+      clearInterval(diskCheckLoop)
+      logger.info(`Received signal ${signal}, shutting down...`)
+      logger.info("exiting...")
+      logger.flush() // pino actually fails to flush, even with awaiting on a callback
+      server.close()
+      process.exit(0)
+    })
+  })
+}
+
+main()
